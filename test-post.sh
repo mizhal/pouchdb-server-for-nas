58dab6059b5e38222e4342081117dddf86e1d500
mizhal <mizhal@gmail.com>
2016-07-16 17:00:10 +0000

    create document for client test

--- test-post.sh
+++ test-post.sh
@@ -2,7 +2,7 @@
 HOST="http://127.0.0.1:3000"
 curl -H 'Content-Type: application/json' \
 	-XPOST "$HOST/db/test" \
-	-d '{"company": "Example, Inc."}'
+	-d '{"_id": "test-key-x", "company": "Example, Inc.", "check": "pandemonium"}'
 curl -H 'Content-Type: application/json' \
 	-XPOST "$HOST/db/test2" \
-	-d '{"company": "Example2, Inc."}'
+	-d '{"_id": "test-key-x", "company": "Example2, Inc.", "check": "pandemonium2"}'
